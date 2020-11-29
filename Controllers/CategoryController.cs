a7e5fa078201071df3e7340f1d978395b02804d5
Maico A Kley <maicokley@hotmail.com>
2020-11-28 23:50:29 -0300

    feat post route categorycontroller

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
 
 [Route("categories")]
 public class CategoryController : ControllerBase
@@ -20,9 +21,9 @@ public class CategoryController : ControllerBase
 
     [HttpPost]
     [Route("")]
-    public string Post()
+    public Category Post([FromBody] Category model)
     {
-        return "POST";
+        return model;
     }
 
     [HttpPut]
