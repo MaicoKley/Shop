70725c90d213b5f485ef1883e57617f103d88ae3
Maico A Kley <maicokley@hotmail.com>
2020-11-29 23:48:53 -0300

    feat ProductController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> GetAction(
+            [FromServices] DataContext context
+        )
+        {
+            var products = await context.Products.Include(x => x.Category).AsNoTracking().ToListAsync();
+            return products;
+        }
+    }
+}
