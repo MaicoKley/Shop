9fa43f10d366193af4d72008dd11aa0dcf0570c7
Maico A Kley <maicokley@hotmail.com>
2020-12-05 20:42:15 -0300

    feat test 3

--- Startup.cs
+++ Startup.cs
@@ -64,8 +64,8 @@ namespace Shop
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-                app.UseDeveloperExceptionPage();
+            //if (env.IsDevelopment())            
+            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
 
